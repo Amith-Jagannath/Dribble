4abf6e3069a0924280f910f9f43e47c19c4b145e
Amith-Jagannath <amithjagannath02@gmail.com>
2023-06-30 17:08:31 +0530

    Dribble -clone

---
+++ app/components/AuthProvider.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthProvider = () => {
+  return (
+    <div><p>Auth</p></div>
+  )
+}
+
+export default AuthProvider
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import AuthProvider from '../components/AuthProvider'
+// import { NavLinks } from '../../constants'
+const NavLinks = [
+  { href: '/', key: 'Inspiration', text: 'Inspiration' },
+  { href: '/', key: 'Find Projects', text: 'Find Projects' },
+  { href: '/', key: 'Learn Development', text: 'Learn Development' },
+  { href: '/', key: 'Career Advancement', text: 'Career Advancement' },
+  { href: '/', key: 'Hire Developers', text: 'Hire Developers' }
+];
+const Navbar = () => {
+  const session =null;
+  return (
+    <nav className="flexBetween navbar
+    ">
+      <div className="flex-1
+      flexStart gap-10
+      ">
+        <Link href='/'>
+          <Image src='/logo.svg' alt='logo' width={115} height={43}></Image>
+        </Link>
+
+        <ul className='xl:flex hidden text-sm gap-7'>
+        
+{NavLinks.map((link) =>(
+  <Link href={link.href} key={link.key}>
+    {link.text}
+  </Link>
+))}
+        </ul>
+      </div>
+      {/* <div className="flexCenter"
+      
+      >
+{ session ? (
+        <>
+       
+        <Link href ="/create-project"></Link>
+        </>
+       
+      ):
+      (
+        <AuthProvider/>
+      )
+    }
+      </div> */}
+      <AuthProvider/>
+    </nav>
+  )
+}
+
+export default Navbar
