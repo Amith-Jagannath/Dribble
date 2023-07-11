41d6f75bf1e1134838ea1f04bcd65f5fce5bae35
Amith-Jagannath <amithjagannath02@gmail.com>
2023-07-11 23:24:33 +0530

    style config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,27 @@
-import './globals.css'
+import "./globals.css";
 // import { Inter } from 'next/font/google'
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export const metadata = {
-  title: 'Flexibble',
-  description: 'developing project',
-}
+  title: "Flexify",
+  description: "developing project",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
         <Navbar></Navbar>
-     
+
         {children}
-        
-        <Footer/>
-        </body>
+
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
