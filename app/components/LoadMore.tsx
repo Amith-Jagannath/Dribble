ae9fb36052838dc25a4c4ce56a596445fbb62c43
Amith-Jagannath <amithjagannath02@gmail.com>
2023-07-12 21:30:37 +0530

    style config

--- app/components/LoadMore.tsx
+++ app/components/LoadMore.tsx
@@ -1,7 +1,63 @@
+// "use client";
+// import React from "react";
+// import Button from "./Button";
+// import { useRouter } from "next/navigation";
+// type Props = {
+//   startCursor: string;
+//   endCursor: string;
+//   hasPreviousPage: boolean;
+//   hasNextPage: boolean;
+// };
+
+// const LoadMore = ({
+//   startCursor,
+//   endCursor,
+//   hasPreviousPage,
+//   hasNextPage,
+// }: Props) => {
+//   const router = useRouter();
+//   const handleNavigation = (direction: string) => {
+//     const currentParams = new URLSearchParams(window.location.search);
+//     if (direction === "next" && hasNextPage) {
+//       currentParams.delete("startcursor");
+//       currentParams.set("endcursor", endCursor);
+//     } else if (direction === "first" && hasPreviousPage) {
+//       currentParams.delete("endcursor");
+//       currentParams.set("startcursor", startCursor);
+//     }
+//     const newSearchParams = currentParams.toString();
+//     const newPathname = `${window.location.pathname}? ${newSearchParams}`;
+//     router.push(newSearchParams);
+//   };
+
+//   return (
+//     <div>
+//       {hasPreviousPage && (
+//         <Button
+//           type="button"
+//           title="First Page"
+//           handleClick={() => handleNavigation("first")}
+//         />
+//       )}
+//       {hasNextPage && (
+//         <Button
+//           type="button"
+//           title="First Page"
+//           handleClick={() => handleNavigation("next")}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default LoadMore;
+
 "use client";
-import React from "react";
-import Button from "./Button";
+
 import { useRouter } from "next/navigation";
+
+import Button from "./Button";
+
 type Props = {
   startCursor: string;
   endCursor: string;
@@ -16,33 +72,35 @@ const LoadMore = ({
   hasNextPage,
 }: Props) => {
   const router = useRouter();
-  const handleNavigation = (direction: string) => {
+
+  const handleNavigation = (type: string) => {
     const currentParams = new URLSearchParams(window.location.search);
-    if (direction === "next" && hasNextPage) {
-      currentParams.delete("startcursor");
-      currentParams.set("endcursor", endCursor);
-    } else if (direction === "first" && hasPreviousPage) {
+
+    if (type === "prev" && hasPreviousPage) {
       currentParams.delete("endcursor");
       currentParams.set("startcursor", startCursor);
+    } else if (type === "next" && hasNextPage) {
+      currentParams.delete("startcursor");
+      currentParams.set("endcursor", endCursor);
     }
+
     const newSearchParams = currentParams.toString();
-    const newPathname = `${window.location.pathname}? ${newSearchParams}`;
-    router.push(newSearchParams);
+    const newPathname = `${window.location.pathname}?${newSearchParams}`;
+
+    router.push(newPathname);
   };
 
   return (
-    <div>
+    <div className="w-full flexCenter gap-5 mt-10">
       {hasPreviousPage && (
         <Button
-          type="button"
           title="First Page"
-          handleClick={() => handleNavigation("first")}
+          handleClick={() => handleNavigation("prev")}
         />
       )}
       {hasNextPage && (
         <Button
-          type="button"
-          title="First Page"
+          title="Next Shots"
           handleClick={() => handleNavigation("next")}
         />
       )}
