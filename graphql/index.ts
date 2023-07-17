b28c38886d93b079959eace64b90c1ce6362056e
Amith-Jagannath <amithjagannath02@gmail.com>
2023-07-17 22:04:19 +0530

    Deploy config

--- graphql/index.ts
+++ graphql/index.ts
@@ -55,8 +55,8 @@ export const createUserMutation = `
 `;
 
 export const projectsQuery = `
-  query getProjects($category: String) {
-    projectSearch(first: 8,filter: {category: {eq: $category}}) {
+  query getProjects() {
+    projectSearch(first: 20) {
       pageInfo {
         hasNextPage
         hasPreviousPage
